f38d00796029d4b58f4dae7d6a35e02f895ca0c5
速度七十迈的蜗牛 <lb18790140172@163.com>
2022-01-09 22:09:34 +0800

    'init'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ msg }}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from "vue";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+  setup() {
+    let msg = ref("this is home");
+    return {
+      msg,
+    };
+  },
+};
 </script>
